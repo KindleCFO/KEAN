74cb891ff0a67e015646e242c774cd3660fab0f3
Andrew Good <andrew.good@kindle-energy.com>
2017-04-17 20:25:59 -0400

    completed load of fuel burn data

--- app/scraping.py
+++ app/scraping.py
@@ -20,3 +20,23 @@ def get_file_pjm(filename):
     ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
 
     ftp.quit()
+
+
+def get_file_pjm_rt(filename):
+
+    ftp = FTP('www.pjm.com')
+    ftp.login()
+    ftp.cwd('pub/account/lmp')
+
+    #filename = '20170402.csv'
+    outfile = sys.stdout
+
+
+    if filename[-4:] == '.zip':
+        localfile = open('data/pjm.zip', 'wb')
+    else:
+        localfile = open('data/pjm-rt.csv', 'wb')
+
+    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+
+    ftp.quit()
